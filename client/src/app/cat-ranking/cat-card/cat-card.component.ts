7c3ee221d1dccd0831341833a925f71c29ddb1fa
Abbas <mhama.abbas@gmail.com>
2020-08-30 13:16:46 +0200

    Create ranking component and childs

--- client/src/app/cat-ranking/cat-card/cat-card.component.ts
+++ client/src/app/cat-ranking/cat-card/cat-card.component.ts
@@ -4,14 +4,20 @@ import { Cat } from 'src/app/models/cat.model';
 @Component({
   selector: 'app-cat-card',
   templateUrl: './cat-card.component.html',
-  styleUrls: ['./cat-card.component.scss']
+  styleUrls: ['./cat-card.component.scss'],
 })
 export class CatCardComponent implements OnInit {
+  @Input() ranking: number;
   @Input() cat: Cat = new Cat();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  /**
+   * Create the css property to display image from url
+   */
+  get backgroundStyle(): any {
+    return { 'background-image': 'url(' + this.cat.imgUrl + ')' };
   }
-
 }
--- client/src/app/cat-ranking/prettiest-cat-card/prettiest-cat-card.component.ts
+++ client/src/app/cat-ranking/prettiest-cat-card/prettiest-cat-card.component.ts
@@ -4,14 +4,19 @@ import { Cat } from 'src/app/models/cat.model';
 @Component({
   selector: 'app-prettiest-cat-card',
   templateUrl: './prettiest-cat-card.component.html',
-  styleUrls: ['./prettiest-cat-card.component.scss']
+  styleUrls: ['./prettiest-cat-card.component.scss'],
 })
 export class PrettiestCatCardComponent implements OnInit {
   @Input() cat: Cat = new Cat();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  /**
+   * Create the css property to display image from url
+   */
+  get backgroundStyle(): any {
+    return { 'background-image': 'url(' + this.cat.imgUrl + ')' };
   }
-
 }
