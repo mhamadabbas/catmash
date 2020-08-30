b4d34949be261d1aae6f5037faa29f7518b98de1
Abbas <mhama.abbas@gmail.com>
2020-08-30 13:17:58 +0200

    Update top button for navigation

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -7,9 +7,32 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  public readonly TOP_BUTTON_CONFIGS = {
+    '/ranking' : {
+      label: 'Changer le classement !',
+      url: 'versus',
+    },
+    '/versus' : {
+      label: 'Voir le classement !',
+      url: 'ranking',
+    },
+  };
+
   constructor(private readonly router: Router) {}
 
-  goToCatRanking(): void {
-    this.router.navigate(['ranking']);
+  /**
+   * Get the top button config
+   * depends on current url
+   */
+  get topButtonConfig(): { label: string; url: string } {
+    const CURRENT_URL = this.router.url;
+    return this.TOP_BUTTON_CONFIGS[CURRENT_URL];
+  }
+
+  /**
+   * Navigate to the configured url
+   */
+  navigate(): void {
+    this.router.navigate([this.topButtonConfig.url]);
   }
 }
